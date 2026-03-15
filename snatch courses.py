fb1dfac3861925cd5592fa17b440daca45608eb1
Wistar-w <1193901958@qq.com>
2026-03-15 21:22:12 +0800

    second

--- snatch courses.py
+++ snatch courses.py
@@ -4,13 +4,12 @@ import random
 
 # 你的 cookies
 cookies = {
-    '******': '8BC063B7625F43AA658724DD80BB0308',
+    'bzb_njw': '8BC063B7625F43AA658724DD80BB0308',
     'JSESSIONID': '1EA52AA30BC0D6ED08A8CAEAFFDDA4B4',
     'SERVERID': '224',
 }
 
-url = 'https://*******/*****/******/bxxkOper?kcid=1510A8E850E9476BADCE030C9D62ED93&cfbs=null&jx0404id=202520262006579&xkzy=&trjf='
-
+url = 'https://jwmis.hhtc.edu.cn/jsxsd/xsxkkc/ggxxkxkOper?kcid=B99489C9F3934F9DACA0703A59AF893F&cfbs=null&jx0404id=202520262005777&xkzy=&trjf='
 # 设置最大尝试次数
 max_attempts = 10
 
@@ -20,17 +19,17 @@ for attempt in range(1, max_attempts + 1):
         # 不设置 headers，仅携带 cookies
         resp = requests.get(url, cookies=cookies, timeout=5)
         resp.raise_for_status()
-        
+
         print(resp.text[:200])  # 打印前200字符
-        
+
         # 根据实际返回内容判断是否成功
         if "成功" in resp.text or "success" in resp.text:
             print("选课成功！停止请求。")
             break
-            
+
     except requests.exceptions.RequestException as e:
         print(f"请求出错：{e}")
         break
-    
+
     # 随机休眠 1~3 秒
     time.sleep(random.uniform(1, 3))
